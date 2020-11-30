b14078c7f9ea685c9b8d310a9b11506ee205bedf
ChangCheWei <10656004@ntub.edu.tw>
2020-11-30 14:46:47 +0800

    跑版修正

--- lib/testMenu.dart
+++ lib/testMenu.dart
@@ -1,16 +1,11 @@
 // import 'dart:html';
 
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'input.dart';
 import 'train.dart';
 import 'home.dart';
-
-
-
 
 class TestMenu extends StatefulWidget {
   @override
@@ -36,99 +31,112 @@ class _TestMenuPageState extends State<TestMenu> {
 
   @override
   Widget build(BuildContext context) {
+
+    this.setState(() {
+      isStraight = MediaQuery.of(context).orientation == Orientation.portrait;
+    });
     Widget homeButton = Flex(
       direction: Axis.horizontal,
       children: <Widget>[
         Expanded(
           flex: 1,
           child: Padding(
-            padding: const EdgeInsets.fromLTRB(25, 25, 0, 0),
-            child: FlatButton(
-              padding: EdgeInsets.zero,
-              onPressed: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context)=>HomeMenuPage()));
-              },
-              child: Image.asset(
-                'images/home.png',
-                height: 50.0,
-                width: 50.0,
-                fit: BoxFit.cover,
+            padding: EdgeInsets.fromLTRB(25, 25, 0, 0),
+            child: AspectRatio(
+              aspectRatio: 1 / 1,
+              child: FlatButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeMenuPage()));
+                },
+                child: Image.asset(
+                  'images/home.png',
+                  fit: BoxFit.fill,
+                ),
               ),
             ),
           ),
         ),
-        Spacer(flex: 5),
+        Spacer(flex: 6),
       ],
     );
 
-    List<Widget> buttons = [
-      Padding(
-          padding: const EdgeInsets.all(20),
-          child: Container(
-            child: FlatButton(
-              padding: EdgeInsets.zero,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => TrainPage()));
-              },
-              child: Stack(
-                alignment: const Alignment(0, 0),
-                children: [
-                  Image.asset(
-                    'images/trainBox.png',
-                    width: 200.0,
-                    fit: BoxFit.fill,
-                  ),
-                  Text('操作教學')
-                ],
+    Widget buttonTrainBox = (Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        child: FlatButton(
+          padding: EdgeInsets.zero,
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => TrainPage()));
+          },
+          child: Stack(
+            alignment: const Alignment(0, 0),
+            children: [
+              Image.asset(
+                'images/trainBox.png',
+                width:isStraight?MediaQuery.of(context).size.height * 0.3:MediaQuery.of(context).size.height * 0.5,
+                fit: BoxFit.cover,
               ),
-            ),
-          )),
-      Padding(
-        padding: EdgeInsets.all(20),
-        child: Center(
-          child: Container(
-            child: FlatButton(
-              padding: EdgeInsets.zero,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => TestInputPage()));
-              },
-              child: Stack(
-                alignment: const Alignment(0, 0),
-                children: [
-                  Image.asset(
-                    'images/testBox.png',
-                    width: 200.0,
-                    fit: BoxFit.fill,
-                  ),
-                  Text('開始檢測')
-                ],
-              ),
+              Text('操作教學')
+            ],
+          ),
+        ),
+      ),
+    ));
+    Widget buttonTestBox = (Padding(
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Container(
+          child: FlatButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => TestInputPage()));
+            },
+            child: Stack(
+              alignment: const Alignment(0, 0),
+              children: [
+                Image.asset(
+                  'images/testBox.png',
+                  width:isStraight?MediaQuery.of(context).size.height * 0.3:MediaQuery.of(context).size.height * 0.5,
+                  fit: BoxFit.cover,
+                ),
+                Text('開始檢測')
+              ],
             ),
           ),
         ),
-      )
-    ];
-    this.setState(() {
-      isStraight = MediaQuery.of(context).orientation == Orientation.portrait;
-    });
+      ),
+    ));
+    
 
     return Container(
       color: Theme.of(context).backgroundColor,
-      child: Column(
-        children: [
-          homeButton,
-          isStraight
-              ? Column(
-                  children: buttons,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                )
-              : Row(
-                  children: buttons,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                )
-        ],
+      child: SizedBox(
+        child: Column(
+          children: <Widget>[homeButton] +
+              (isStraight
+                  ? [
+                      Container(
+                        height:MediaQuery.of(context).size.height * 0.85,
+                        child: Flex(
+                          direction: Axis.vertical,
+                          children: [
+                            Expanded(flex:1,child:buttonTrainBox),
+                            Expanded(flex:1,child:buttonTestBox),
+                          ],
+                        ),
+                      ),
+                    ]
+                  : [
+                      Row(
+                        children: [buttonTrainBox, buttonTestBox],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                    ]),
+        ),
       ),
     );
   }
