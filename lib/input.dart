26ad66ba9eb6c89e472fb7ca054249621e01bfdf
yusiang <10656010@ntub.edu.tw>
2020-12-09 17:34:17 +0800

    調整畫面
    輸入頁:縣市順序依南到北
    歡迎頁自動跳頁
    操作說明頁下方加按鈕跳頁到測試頁

--- lib/input.dart
+++ lib/input.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home.dart';
+import 'package:flutter_app/screenArgs.dart';
 import 'test.dart';
 import 'package:flutter_better_camera/camera.dart';
 import 'package:date_format/date_format.dart';
@@ -55,25 +56,25 @@ class InputPageState extends State<InputWidget> {
   ];
   List<String> areas = [
     "基隆市",
-    "南投縣",
-    "屏東縣",
     "台北市",
-    "雲林縣",
-    "宜蘭縣",
     "新北市",
-    "嘉義縣",
-    "花蓮縣",
     "桃園市",
-    "台南市",
-    "台東縣",
     "新竹縣",
-    "高雄市",
-    "澎湖縣",
     "新竹市",
-    "台中市",
-    "金門縣",
     "苗栗縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義縣",
+    "台南市",
+    "高雄市",
+    "台中市",
     "彰化縣",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "台東縣",
+    "澎湖縣",
+    "金門縣",
     "其他"
   ];
   String item = "";
@@ -96,172 +97,178 @@ class InputPageState extends State<InputWidget> {
         : MediaQuery.of(context).size.width;
 
     main();
-    return Container(
-      height: sizeHeight,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-         
-          Flex(
-            direction: Axis.horizontal,
-            
-            children: <Widget>[
-              Expanded(
-                flex: isStraight ? 2 : 1,
-                child: FlatButton(
-                  
-                  padding: EdgeInsets.zero,
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => HomeMenuPage()));
-                  },
-                  child: Image.asset(
-                    'images/home.png',
-                    height: 45,
-                    width: 45,
-                    fit: BoxFit.cover,
-                  ),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 245, 227, 1),
+      body: Container(
+        child: Center(
+          child: Container(
+            height: sizeHeight,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Flex(
+                  direction: Axis.horizontal,
+                  children: <Widget>[
+                    Expanded(
+                      flex: isStraight ? 2 : 1,
+                      child: FlatButton(
+                        padding: EdgeInsets.zero,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomeMenuPage()));
+                        },
+                        child: Image.asset(
+                          'images/home.png',
+                          height: 45,
+                          width: 45,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Spacer(flex: 8),
+                  ],
                 ),
-              ),
-              Spacer(flex: 8),
-            ],
-          ),
-          Flex(
-            direction: Axis.horizontal,
-            children: [
-              isStraight ? Container() : Spacer(flex: 2),
-              Expanded(
-                child: Image.asset(
-                  "images/note.png",
-                  height: 50,
+                Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    isStraight ? Container() : Spacer(flex: 2),
+                    Expanded(
+                      child: Image.asset(
+                        "images/note.png",
+                        height: 50,
+                      ),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Text(
+                        "檢測小筆記",
+                        // style: Theme.of(context).textTheme.headline1,
+                      ),
+                      flex: 3,
+                    ),
+                    isStraight ? Container() : Spacer(flex: 2)
+                  ],
                 ),
-                flex: 1,
-              ),
-              Expanded(
-                child: Text(
-                  "檢測小筆記",
-                  style: Theme.of(context).textTheme.headline1,
+                Row(
+                  // mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      padding: EdgeInsets.all(10),
+                      decoration: boxDecoration,
+                      child: Text("檢測蔬果"),
+                    ),
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      padding: EdgeInsets.all(0),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: boxDecoration,
+                      child: DropdownButton<String>(
+                        hint: Container(
+                          width: 150,
+                          child: Text(item == "" ? "請選擇檢測蔬果" : item),
+                        ),
+                        items: items.map((String value) {
+                          return new DropdownMenuItem<String>(
+                              value: value, child: Text(value));
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            item = value;
+                          });
+                        },
+                      ),
+                    )
+                  ],
                 ),
-                flex: 3,
-              ),
-              isStraight ? Container() : Spacer(flex: 2)
-            ],
-          ),
-          Row(
-            // mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                padding: EdgeInsets.all(10),
-                decoration: boxDecoration,
-                child: Text("檢測蔬果"),
-              ),
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                padding: EdgeInsets.all(0),
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                decoration: boxDecoration,
-                child: DropdownButton<String>(
-                  hint: Container(
-                    width: 150,
-                    child: Text(item == "" ? "請選擇檢測蔬果" : item),
-                  ),
-                  items: items.map((String value) {
-                    return new DropdownMenuItem<String>(
-                        value: value, child: Text(value));
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      item = value;
-                    });
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: boxDecoration,
+                      child: Text("來自/購買地區"),
+                    ),
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      padding: EdgeInsets.all(0),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: boxDecoration,
+                      child: DropdownButton<String>(
+                        hint: Container(
+                          width: 150,
+                          child: Text(area == "" ? "請選擇購買地點" : area),
+                        ),
+                        items: areas.map((String value) {
+                          return new DropdownMenuItem<String>(
+                              value: value, child: Text(value));
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            area = value;
+                          });
+                        },
+                      ),
+                    )
+                  ],
                 ),
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                decoration: boxDecoration,
-                child: Text("來自/購買地區"),
-              ),
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                padding: EdgeInsets.all(0),
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                decoration: boxDecoration,
-                child: DropdownButton<String>(
-                  hint: Container(
-                    width: 150,
-                    child: Text(area == "" ? "請選擇購買地點" : area),
-                  ),
-                  items: areas.map((String value) {
-                    return new DropdownMenuItem<String>(
-                        value: value, child: Text(value));
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      area = value;
-                    });
-                  },
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: boxDecoration,
+                      child: Text("時間"),
+                    ),
+                    Container(
+                      width: sizeWidth * 0.38,
+                      height: sizeHeight * 0.14,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      decoration: boxDecoration,
+                      child: Text(formatDate(DateTime.now(),
+                          [yyyy, '-', mm, '-', dd, " ", HH, ':', nn, ':', ss])),
+                    ),
+                  ],
                 ),
-              )
-            ],
+                FlatButton(
+                    onPressed: () {
+                      if (area != "" && item != "") {
+                        // Navigator.pushNamed(context, "/test",
+                        //     arguments: ScreenArgs.first(1, cameras));
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => CameraApp(1,cameras),),);
+                      } else {
+                        Fluttertoast.showToast(
+                            msg: "請選擇蔬果類型與購買地點",
+                            toastLength: Toast.LENGTH_SHORT,
+                            gravity: ToastGravity.BOTTOM,
+                            timeInSecForIosWeb: 1,
+                            backgroundColor: Colors.grey,
+                            textColor: Colors.white,
+                            fontSize: 16.0);
+                      }
+                    },
+                    child: Text("確定"))
+              ],
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                decoration: boxDecoration,
-                child: Text("時間"),
-              ),
-              Container(
-                width: sizeWidth*0.38,
-                height: sizeHeight*0.14,
-                padding: EdgeInsets.all(10),
-                margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
-                decoration: boxDecoration,
-                child: Text(formatDate(DateTime.now(),
-                    [yyyy, '-', mm, '-', dd, " ", HH, ':', nn, ':', ss])),
-              ),
-            ],
-          ),
-          FlatButton(
-              onPressed: () {
-                if (area != "" && item != "") {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => CameraApp(1, cameras)));
-                } else {
-                  Fluttertoast.showToast(
-                      msg: "請選擇蔬果類型與購買地點",
-                      toastLength: Toast.LENGTH_SHORT,
-                      gravity: ToastGravity.BOTTOM,
-                      timeInSecForIosWeb: 1,
-                      backgroundColor: Colors.grey,
-                      textColor: Colors.white,
-                      fontSize: 16.0);
-                }
-              },
-              child: Text("確定"))
-        ],
+        ),
       ),
     );
   }
--- lib/result.dart
+++ lib/result.dart
@@ -1,51 +1,45 @@
 // import 'dart:html';
+import 'package:flutter_app/screenArgs.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home.dart';
 
-// double rate;
-String rate;
-String content;
-double num;
+String rate = "0%";
+String content = "合格";
+double result;
 
 class ResultPage extends StatelessWidget {
-  ResultPage(double n) {
-    num = n;
-    rate = num.floor().toString() + "%";
-    if (num <= 35)
+  @override
+  Widget build(BuildContext context) {
+    final ScreenArgs args = ModalRoute.of(context).settings.arguments;
+    result = args.result;
+    rate = result.floor().toString() + "%";
+    if (result <= 35)
       content = "合格";
-    else if (num <= 45)
+    else if (result <= 45)
       content = "通知供應單位延期採收\n追蹤農民用藥";
     else
       content = "銷毀或\n將樣品送衛生局複檢";
-  }
-
-  ResultPage.empty() {
-    rate = "0%";
-    content = "合格";
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
+          backgroundColor: Color.fromRGBO(255, 245, 227, 1),
+          fontFamily: "openhuninn",
+          floatingActionButtonTheme: FloatingActionButtonThemeData(
             backgroundColor: Color.fromRGBO(255, 245, 227, 1),
-            fontFamily: "openhuninn",
-            floatingActionButtonTheme: FloatingActionButtonThemeData(
-              backgroundColor: Color.fromRGBO(255, 245, 227, 1),
-              shape: RoundedRectangleBorder(),
-              elevation: 0,
+            shape: RoundedRectangleBorder(),
+            elevation: 0,
+          ),
+          textTheme: TextTheme(
+            bodyText1: TextStyle(
+              fontSize: 30,
+              color: result < 35
+                  ? Colors.green
+                  : (result < 45 ? Colors.amber : Colors.red),
+              decoration: TextDecoration.none,
             ),
-            textTheme: TextTheme(
-              bodyText1: TextStyle(
-                fontSize: 30,
-                color: num < 35
-                    ? Colors.green
-                    : (num < 45 ? Colors.amber : Colors.red),
-                decoration: TextDecoration.none,
-              ),
-            )),
+          ),
+        ),
         home: Result());
   }
 }
@@ -60,87 +54,80 @@ class ResultState extends State<Result> {
     this.setState(() {
       isStraight = MediaQuery.of(context).orientation == Orientation.portrait;
     });
-    // print(insideR<35);
-    // if(insideR<=35) str="合格";
-    // else if(insideR<=45) str="通知供應單位延期採收\n追蹤農民用藥";
-    // else str="銷毀或\n將樣品送衛生局複檢";
+
     if (isStraight) {
       return Container(
         color: Color.fromRGBO(255, 245, 227, 1),
-        child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(children: [
+          Row(
             children: [
-              Row(
-                children: [
-                  FloatingActionButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeMenuPage()));
-                    },
-                    child: Image.asset("images/home.png"),
-                    heroTag: "home",
-                  ),
-                  FloatingActionButton(
-                    onPressed: _launchURLCustomerService,
-                    child: Image.asset("images/customerService.png"),
-                    heroTag: "customerService",
-                  ),
-                ],
+              FloatingActionButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeMenuPage()));
+                },
+                child: Image.asset("images/home.png"),
+                heroTag: "home",
               ),
+              FloatingActionButton(
+                onPressed: _launchURLCustomerService,
+                child: Image.asset("images/customerService.png"),
+                heroTag: "customerService",
+              ),
+            ],
+          ),
+          Stack(
+            alignment: const Alignment(0.0, -0.1),
+            children: [
+              Image.asset("images/rateBox.png"),
+              Text(
+                rate,
+                style: new TextStyle(
+                  fontSize: 50,
+                  decoration: TextDecoration.none,
+                  color: Color.fromRGBO(153, 87, 37, 1),
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: []),
               Stack(
-                alignment: const Alignment(0.0, -0.1),
+                alignment: const Alignment(0.0, 0.2),
                 children: [
-                  Image.asset("images/rateBox.png"),
+                  Stack(
+                    alignment: const Alignment(0.8, -0.8),
+                    children: [
+                      Image.asset("images/report.png"),
+                      Image.asset("images/share.png"),
+                    ],
+                  ),
                   Text(
-                    rate,
-                    style: new TextStyle(
-                      fontSize: 50,
+                    content,
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: result < 35
+                          ? Colors.green
+                          : (result < 45 ? Colors.amber : Colors.red),
                       decoration: TextDecoration.none,
-                      color: Color.fromRGBO(153, 87, 37, 1),
                     ),
+
+                    // rate < 35
+                    //     ? "合格"
+                    //     : rate < 45
+                    //         ? ""
+                    //         : "銷毀或\n將樣品送衛生局複檢",
+                    // style: new TextStyle(fontSize: 45),
                   )
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: []),
-                  Stack(
-                    alignment: const Alignment(0.0, 0.2),
-                    children: [
-                      Stack(
-                        alignment: const Alignment(0.8, -0.8),
-                        children: [
-                          Image.asset("images/report.png"),
-                          Image.asset("images/share.png"),
-                        ],
-                      ),
-                      Text(
-                        content,
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: num < 35
-                              ? Colors.green
-                              : (num < 45 ? Colors.amber : Colors.red),
-                          decoration: TextDecoration.none,
-                        ),
-
-                        // rate < 35
-                        //     ? "合格"
-                        //     : rate < 45
-                        //         ? ""
-                        //         : "銷毀或\n將樣品送衛生局複檢",
-                        // style: new TextStyle(fontSize: 45),
-                      )
-                    ],
-                  ),
-                ],
-              ),
-            ]),
+            ],
+          ),
+        ]),
       );
     } else {
       return Container(
@@ -149,7 +136,9 @@ class ResultState extends State<Result> {
 
             // mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(padding: EdgeInsets.fromLTRB(15, 10, 0, 0),),
+              Padding(
+                padding: EdgeInsets.fromLTRB(15, 10, 0, 0),
+              ),
               Row(
                 children: [
                   FloatingActionButton(
@@ -204,9 +193,9 @@ class ResultState extends State<Result> {
                         content,
                         style: TextStyle(
                           fontSize: 30,
-                          color: num < 35
+                          color: result < 35
                               ? Colors.green
-                              : (num < 45 ? Colors.amber : Colors.red),
+                              : (result < 45 ? Colors.amber : Colors.red),
                           decoration: TextDecoration.none,
                         ),
                         // (rate<35
