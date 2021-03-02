6319ec4d9a689c2b6d4805fd8112789756b64874
yusiang <10656010@ntub.edu.tw>
2021-03-02 20:44:48 +0800

    新增 - 鏡頭檢查紐

--- lib/testMenu.dart
+++ lib/testMenu.dart
@@ -3,10 +3,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fun_Heart_eat/check.dart';
+import 'package:fun_Heart_eat/dataBean.dart';
 import 'input.dart';
 import 'train.dart';
 import 'home.dart';
 import 'customeItem.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter_better_camera/camera.dart';
 
 class TestMenu extends StatefulWidget {
   @override
@@ -16,6 +20,7 @@ class TestMenu extends StatefulWidget {
 class TestMenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getCameras();
     return MaterialApp(
       home: TestMenu(),
       debugShowCheckedModeBanner: false,
@@ -46,6 +51,7 @@ class _TestMenuPageState extends State<TestMenu> {
       iconSize = isStraight ? sizeWidth / 7 : sizeHeight * 0.15;
       imgW = isStraight ? sizeHeight * 0.3 : sizeWidth * 0.3;
     });
+
     Widget buttonTrainBox = Padding(
       padding: EdgeInsets.all(10),
       child: Container(
@@ -106,7 +112,7 @@ class _TestMenuPageState extends State<TestMenu> {
     );
 
     return Container(
-      color:Theme.of(context).backgroundColor,
+      color: Theme.of(context).backgroundColor,
       child: SafeArea(
         child: Container(
           padding: EdgeInsets.all(5),
@@ -133,7 +139,15 @@ class _TestMenuPageState extends State<TestMenu> {
                               fit: BoxFit.cover,
                             ),
                           ),
-                        )
+                        ),
+                        CustomButton("檢測鏡頭", () {
+                          DataBean d = new DataBean();
+                          d.cameras = cameras;
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CheckPage(d)));
+                        }),
                       ],
                     )
                   ] +
@@ -179,4 +193,20 @@ class _TestMenuPageState extends State<TestMenu> {
       ),
     );
   }
+}
+
+List<CameraDescription> cameras = [];
+
+Future<void> getCameras() async {
+  if (await Permission.camera.request().isGranted) {
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+
+      cameras = await availableCameras();
+    } on CameraException catch (e) {
+      logError(e.code, e.description);
+    }
+  }
+
+  // Fetch the available cameras before initializing the app.
 }
